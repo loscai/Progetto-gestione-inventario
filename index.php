e3c600dc27b33492facb998c9407f964aae14098
loscai <christiancolombo2k5@gmail.com>
2024-12-10 17:34:31 +0100

    Spostamento dei file fuori da una cartella perché rende tutto scomodo

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FNFK</title>
+</head>
+<body>
+<h1>BENVENUTO NEL NOSTRO SITO DI ACQUISTI ALL'INGROSSO</h1>
+
+
+<button onclick="window.location.href='login.php';">LOGIN</button>
+<br>
+<button onclick="window.location.href='registrazione.php';">REGISTRATI</button>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+</head>
+
+<body>
+
+    <?php
+    if (isset($_GET["messaggio"]))
+        echo $_GET["messaggio"]; 
+    ?>
+
+    <form action="gestoreLogin.php" method="POST">
+
+        <h4>Username: </h4> <input type="text" name="username" required>
+        <br>
+        <h4>Password: </h4> <input type="password" name="password" required>
+        <br>
+
+        <input type="submit" value="INVIA">
+        <button onclick="window.location.href='recuperaPassword.php';">RECUPERA PASSWORD</button>
+
+    </form>
+</body>
+
+</html>
