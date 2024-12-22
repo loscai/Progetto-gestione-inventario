828c4531ed9a7de33eca0e1b94d932725bc473a6
loscai <christiancolombo2k5@gmail.com>
2024-12-22 14:22:49 +0100

    creazione classe prodotto e file prodotti.csv

---
+++ classes/Prodotto.php
@@ -0,0 +1,33 @@
+<?php
+
+class Prodotto
+{
+    private $IDprodotto;
+    private $Nome;
+    private $Descrizione;
+    private $Fornitore;
+    private $Prezzo;
+    private $pathImmagine;
+
+    public function __construct($IDprodotto, $Nome, $Descrizione, $Fornitore, $Prezzo, $pathImmagine)
+    {
+        $this->IDprodotto = $IDprodotto;
+        $this->Nome = $Nome;
+        $this->Descrizione = $Descrizione;
+        $this->Fornitore = $Fornitore;
+        $this->Prezzo = $Prezzo;
+        $this->pathImmagine = $pathImmagine;
+    }
+
+    public function toCSV(){
+        file_put_contents("../prodotti/datas/prodotti.csv",
+        $this->IDprodotto . ";" .
+        $this->Nome . ";" .
+        $this->Descrizione . ";". 
+        $this->Fornitore . ";". 
+        $this->Prezzo . ";" .
+        $this->pathImmagine . "\r\n");
+    }
+}
+
+?>
