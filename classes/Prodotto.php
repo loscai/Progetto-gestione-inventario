81c3f436cef2858a28d9047bd02365716edfb465
OraOtak <125869633+Discord-104@users.noreply.github.com>
2024-12-22 14:53:19 +0100

    sistemazione progetto

--- classes/Prodotto.php
+++ classes/Prodotto.php
@@ -1,33 +1,31 @@
 <?php
-
-class Prodotto
-{
-    private $IDprodotto;
-    private $Nome;
-    private $Descrizione;
-    private $Fornitore;
-    private $Prezzo;
-    private $pathImmagine;
-
-    public function __construct($IDprodotto, $Nome, $Descrizione, $Fornitore, $Prezzo, $pathImmagine)
+    class Prodotto
     {
-        $this->IDprodotto = $IDprodotto;
-        $this->Nome = $Nome;
-        $this->Descrizione = $Descrizione;
-        $this->Fornitore = $Fornitore;
-        $this->Prezzo = $Prezzo;
-        $this->pathImmagine = $pathImmagine;
-    }
+        private $IDprodotto;
+        private $Nome;
+        private $Descrizione;
+        private $Fornitore;
+        private $Prezzo;
+        private $pathImmagine;
 
-    public function toCSV(){
-        file_put_contents("../prodotti/datas/prodotti.csv",
-        $this->IDprodotto . ";" .
-        $this->Nome . ";" .
-        $this->Descrizione . ";". 
-        $this->Fornitore . ";". 
-        $this->Prezzo . ";" .
-        $this->pathImmagine . "\r\n");
-    }
-}
+        public function __construct($IDprodotto, $Nome, $Descrizione, $Fornitore, $Prezzo, $pathImmagine)
+        {
+            $this->IDprodotto = $IDprodotto;
+            $this->Nome = $Nome;
+            $this->Descrizione = $Descrizione;
+            $this->Fornitore = $Fornitore;
+            $this->Prezzo = $Prezzo;
+            $this->pathImmagine = $pathImmagine;
+        }
 
+        public function toCSV(){
+            file_put_contents("../prodotti/datas/prodotti.csv",
+            $this->IDprodotto . ";" .
+            $this->Nome . ";" .
+            $this->Descrizione . ";". 
+            $this->Fornitore . ";". 
+            $this->Prezzo . ";" .
+            $this->pathImmagine . "\r\n");
+        }
+    }
 ?>
